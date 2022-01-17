0838b230315cca88774f1630617c996ec9398db3
Dark❶ <25451052+Dark1z@users.noreply.github.com>
2022-01-17 23:05:22 +0530

    MAS v3.2.6
    - phpBB CDB changes
    - CopyRight Year to ForEver
    
    Signed-off-by: Dark❶ <25451052+Dark1z@users.noreply.github.com>

--- language/en/info_acp_mas.php
+++ language/en/info_acp_mas.php
@@ -3,7 +3,7 @@
  *
  * Member Avatar & Status [MAS]. An extension for the phpBB Forum Software package.
  *
- * @copyright (c) 2018-2021, Dark❶ [dark1]
+ * @copyright (c) 2018-forever, Dark❶ [dark1]
  * @license GNU General Public License, version 2 (GPL-2.0-only)
  *
  *
